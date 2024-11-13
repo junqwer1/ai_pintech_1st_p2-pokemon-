129dcc51c890a2eac8b12379016c9258f1af96a8
junqwer1 <zzang12358@naver.com>
2024-11-13 10:57:11 +0900

    구성

---
+++ src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -0,0 +1,21 @@
+package org.koreait.global.configs;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    /*
+    * 정적 경로 설정, CSS, JS, 이미지
+    *
+    * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+
+}
