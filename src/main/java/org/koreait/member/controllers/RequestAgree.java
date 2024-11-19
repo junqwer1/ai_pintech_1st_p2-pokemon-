fbf86208aaf3b80e9669b61f8e07b6c4dbbd4790
junqwer1 <zzang12358@naver.com>
2024-11-19 16:47:31 +0900

    구성

--- src/main/java/org/koreait/member/controllers/RequestAgree.java
+++ src/main/java/org/koreait/member/controllers/RequestAgree.java
@@ -1,5 +1,6 @@
 package org.koreait.member.controllers;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -8,8 +9,14 @@ import java.util.List;
 @Data
 public class RequestAgree{
 
-    @NotNull
-    private boolean[] requiredTerms; // 필수 약관 동의 여부 / 필수는 반드시 모두 선택 - 갯수 체크만
+    @AssertTrue
+    private boolean requiredTerms1; // 필수 약관 동의 여부 /
+
+    @AssertTrue
+    private boolean requiredTerms2; // 필수 약관 동의 여부 /
+
+    @AssertTrue
+    private boolean requiredTerms3; // 필수 약관 동의 여부 /
 
     private List<String> optionalTerms; // 선택 약관 동의 여부 - 선택약관은 어떤 약관인지를 구분할 수 있어야 함
 
