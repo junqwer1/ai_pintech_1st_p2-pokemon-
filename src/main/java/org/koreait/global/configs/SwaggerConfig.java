7e7e4711244dcfe515f80093baeb73ea0e694b95
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-05 15:36:58 +0900

    파일 공동 모듈

---
+++ src/main/java/org/koreait/global/configs/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package org.koreait.global.configs;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+
+        return GroupedOpenApi.builder()
+                .group("포켓몬 도감 API")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
