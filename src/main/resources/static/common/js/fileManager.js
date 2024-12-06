da6d77564c0a17e6363d4bbdf717d79b4ced3c93
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-06 18:25:32 +0900

    Update fileManager.js

--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -1,3 +1,29 @@
+var commonLib = commonLib ?? {};
+commonLib.fileManager = {
+    /*
+        파일 업로드 처리
+    */
+    upload(files, gid, location, single, imageOnly) {
+        try{
+            if(!files || files.length == 0) {
+                throw new Error("파일을 선택하세요.");
+            }
+
+            if(imageOnly) { // 이미지만 업로드 하는 경우
+                for (const file of files) {
+                    if (file.type.indexOf("image/") === -1) { // 이미지가 아닌 파일인 경우
+                        throw new Error("이미지 형식이 아닙니다");
+                    }
+                }
+            }
+        } catch (err) {
+            alert(err.message);
+            console.error(err);
+        }
+    }
+
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     const fileUploads = document.getElementsByClassName("file-upload");
     const fileEl = document.createElement("input");
@@ -19,7 +45,11 @@ window.addEventListener("DOMContentLoaded", function() {
 
 //    파일 선택시 - change 이벤트 발생
     fileEl.addEventListener("change", function(e) {
-        // e.currentTarget, e.target
-        console.log(e.currentTarget.files);
+         const files = e.currentTarget.files;
+         const {gid, location, single, imageOnly} = fileEl
+//        console.log(e.currentTarget.files);
+
+        const { fileManager } = commonLib;
+        fileManager.upload(files, gid, location, single, imageOnly);
     });
 });
