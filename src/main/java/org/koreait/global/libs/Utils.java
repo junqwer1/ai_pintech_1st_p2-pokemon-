8b59e9f7e34c034a3a1795b1ca0cf9e997a643bb
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-10 15:34:41 +0900

    .

--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -2,16 +2,18 @@ package org.koreait.global.libs;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.koreait.file.controllers.RequestThumb;
+import org.koreait.file.entities.FileInfo;
+import org.koreait.file.services.FileInfoService;
+import org.koreait.file.services.ThumbnailService;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
@@ -20,6 +22,8 @@ public class Utils {
 
     private final HttpServletRequest request;
     private final MessageSource messageSource;
+    private final FileInfoService fileInfoService;
+    e;
 
     public boolean isMobile() {
 
@@ -92,6 +96,26 @@ public class Utils {
         } finally {
             ms.setUseCodeAsDefaultMessage(true);
         }
+    }
 
+    /**
+     * 이미지 출력
+     * @param mode - image : 이미지 태그로 출력, background : 배경 이미지 형태 출력
+     * */
+    public String showImage(Long seq, String url, int width, int height, String mode) {
+        try {
+            if (seq != null && seq > 0L){
+                FileInfo item = fileInfoService.get(seq);
+                if (!item.getContentType().contains("image/")) {
+
+                }
+            } else if (StringUtils.hasText(url)) {
+
+            }
+
+            mode = Objects.requireNonNullElse(mode, "image");
+        } catch (Exception e) {}
+
+        return "";
     }
 }
