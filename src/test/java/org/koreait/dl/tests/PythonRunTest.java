820424c6722c70dd035ef569dbb06a1c867f2a2a
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-10 11:08:44 +0900

    .

--- src/test/java/org/koreait/dl/tests/PythonRunTest.java
+++ src/test/java/org/koreait/dl/tests/PythonRunTest.java
@@ -12,7 +12,7 @@ public class PythonRunTest {
 
     @Test
     void test1() throws Exception {
-        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/오준학/recommend/train.py", "http://localhost3000/api/dl/data");
+        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/recommend/train.py", "http://localhost3000/api/dl/data");
         Process process = builder.start();
         BufferedReader reader = process.inputReader();
         reader.lines().forEach(System.out::println);
@@ -20,10 +20,11 @@ public class PythonRunTest {
         System.out.println(exitCode);
     }
 
+    @Test
     void test2() throws Exception{
 //        python predict.py "[[ 1.23151481,  0.88790998,  1.6140196,   0.94127238,  1.6784415,   1.38504672, -1.57161094, -0.65513703,  0.99961796 , -0.80484811]]"
 
-        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/오준학/recommend/predict.py", "[[ 1.23151481,  0.88790998,  1.6140196,   0.94127238,  1.6784415,   1.38504672, -1.57161094, -0.65513703,  0.99961796 , -0.80484811]]");
+        ProcessBuilder builder = new ProcessBuilder("C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python39\\python.exe", "D:/recommend/predict.py", "[[ 1.23151481,  0.88790998,  1.6140196,   0.94127238,  1.6784415,   1.38504672, -1.57161094, -0.65513703,  0.99961796 , -0.80484811]]");
         Process process = builder.start();
         BufferedReader reader = process.inputReader();
         reader.lines().forEach(System.out::println);
