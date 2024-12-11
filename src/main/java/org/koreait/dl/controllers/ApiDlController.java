d91ec4256eb44749a19903fdda164f5de74f885d
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-11 11:19:46 +0900

    .

--- src/main/java/org/koreait/dl/controllers/ApiDlController.java
+++ src/main/java/org/koreait/dl/controllers/ApiDlController.java
@@ -2,13 +2,12 @@ package org.koreait.dl.controllers;
 
 import org.koreait.dl.entities.TrainItem;
 import org.koreait.dl.services.PredictService;
+import org.koreait.dl.services.TrainService;
 import org.koreait.global.rests.JSONData;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 @Profile("dl")
 @RestController
@@ -16,25 +15,12 @@ import java.util.stream.IntStream;
 public class ApiDlController {
 
     private PredictService predictService;
+    private TrainService trainService;
 
     @GetMapping("/data")
-    public List<TrainItem> sendData() {
-        Random random = new Random();
-        List<TrainItem> items = IntStream.range(0, 1000)
-                .mapToObj(i -> TrainItem.builder()
-                        .item1(random.nextInt())
-                        .item2(random.nextInt())
-                        .item3(random.nextInt())
-                        .item4(random.nextInt())
-                        .item5(random.nextInt())
-                        .item6(random.nextInt())
-                        .item7(random.nextInt())
-                        .item8(random.nextInt())
-                        .item9(random.nextInt())
-                        .item10(random.nextInt())
-                        .result(random.nextInt(4))
-                        .build()
-                ).toList();
+    public List<TrainItem> sendData(@RequestParam(name = "mode", required = false) String mode) {
+        List<TrainItem> items = trainService.getList(mode != null && mode.equals("ALL"));
+
         return items;
     }
 
