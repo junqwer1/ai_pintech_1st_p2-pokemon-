b7c41ba84b9aea4dd440ce2057d8d73b9a8cb4a7
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-16 17:04:56 +0900

    .

--- src/main/java/org/koreait/email/controllers/RequestEmail.java
+++ src/main/java/org/koreait/email/controllers/RequestEmail.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class RequestEmail {
     private List<String> to; // 받는 쪽 이메일
     private List<String> cc; // 참조
+    private List<String> bcc; // 숨은 참조
     private String subject; // 메일 제목
     private String content; // 메일 내용
 }
