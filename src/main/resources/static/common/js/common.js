6d0b5b34f82bf97a6690ed897a2f0f9c820e79b0
junqwer1 <zzang12358@naver.com>
2024-12-22 04:31:03 +0900

    Update common.js

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -51,7 +51,7 @@ commonLib.ajaxLoad = function(url, callback, method = 'GET', data, headers) {
     return new Promise((resolve, reject) => {
         fetch(url, options)
             .then(res => {
-                if (res.status !== 204)
+                if (res.status !== 204) //204 응답 데이터 없음
                     return res.json()
                 else {
                     resolve();
