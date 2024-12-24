a298291e848256d640cbff53ec5714d577e10bb5
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-24 16:18:57 +0900

    .

---
+++ src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
@@ -0,0 +1,50 @@
+package org.koreait.global.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.koreait.global.entities.SiteConfig;
+import org.koreait.global.services.CodeValueService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class CommonInterceptor implements HandlerInterceptor {
+
+    private final CodeValueService codeValueService;
+    private List<String> excludeExtensions = List.of(
+            ".css",
+            ".js",
+            ".png",
+            ".gif",
+            ".jpg",
+            ".jpeg",
+            ".pdf",
+            ".txt"
+    );
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        setSiteConfig(modelAndView, request);
+
+    }
+
+    /* 사이트 설정 */
+    private void setSiteConfig(ModelAndView mv, HttpServletRequest request) {
+        String uri = request.getRequestURI().toLowerCase();
+        boolean matched = excludeExtensions.stream().anyMatch(uri::contains);
+        if (!matched) {
+            return;
+        }
+
+        SiteConfig config = Objects.requireNonNullElseGet(codeValueService.get("siteConfig", SiteConfig.class), SiteConfig::new);
+
+        mv.addObject("siteConfig", config);
+    }
+}
