bcaffe86a85f4592df25602c46cbc8212fdb8e68
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-27 11:06:45 +0900

    .

--- src/main/java/org/koreait/admin/member/controllers/MemberController.java
+++ src/main/java/org/koreait/admin/member/controllers/MemberController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.admin.global.menu.MenuDetail;
 import org.koreait.admin.global.menu.SubMenus;
 import org.koreait.global.annotations.ApplyErrorPage;
+import org.koreait.global.libs.Utils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +18,49 @@ import java.util.List;
 @Controller("adminMemberController")
 public class MemberController implements SubMenus {
 
+    private final Utils utils;
+
     @ModelAttribute("menuCode")
     public String menuCode() {
         return "member";
     }
 
+    @GetMapping({"", "/list"})
+    public String list(@ModelAttribute MemberSearch search, Model model) {
+        commonProcess("list", model);
+
+        return "admin/member/list";
+    }
+
+    /**
+     * 회원목록 수정 처리
+     * @param model
+     * @return
+     */
+    @PatchMapping("/list")
+    public String listPs(@RequestParam(name = "chk", required = false) List<Integer> chks, Model model) {
+
+        utils.showSessionMessage("수정하였습니다");
+        model.addAttribute("script", "parent.location.reload();");
+        return "common/_execute_script";
+    }
+
+    /**
+     * 공통 처리 부분
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "list";
+        String pageTitle = "";
+        if (mode.equals("list")) {
+            pageTitle = "회원목록";
+        }
+
+        pageTitle += " - 회원관리";
+
+        model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("subMenuCode", mode);
+    }
 }
