0d16682c83059788b8de55a1105d1ac7ccb3ac59
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-02 16:06:48 +0900

    .

--- src/main/java/org/koreait/dl/services/SentimentService.java
+++ src/main/java/org/koreait/dl/services/SentimentService.java
@@ -3,12 +3,15 @@ package org.koreait.dl.services;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Lazy
 @Service
 @Profile("dl")
 public class SentimentService {
@@ -27,7 +30,7 @@ public class SentimentService {
 
     public double[] predict(List<String> items) {
         try {
-            String data = om.writeValueAsString(items);
+            String data = String.join("__", items);
 
             ProcessBuilder builder = new ProcessBuilder(runPath, scriptPath + "naver.py", bertPath, data); //파이썬 경로, script 경로, bert경로
             Process process = builder.start();
--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -94,15 +94,17 @@ window.addEventListener("DOMContentLoaded", function() {
                 fileEl.removeEventListener("change", fileEventHandler);
                 fileEl.addEventListener("change", fileEventHandler);
 
-                function fileEventHandler(e) {
-                    const files = e.currentTarget.files;
-                    const {gid, location, single, imageOnly, done} = fileEl;
-            //        console.log(e.currentTarget.files);
 
-                    const { fileManager } = commonLib;
-                    fileManager.upload(files, gid, location, single, imageOnly, done);
-                }
         });
+
+        function fileEventHandler(e) {
+            const files = e.currentTarget.files;
+            const {gid, location, single, imageOnly, done} = fileEl;
+    //        console.log(e.currentTarget.files);
+
+            const { fileManager } = commonLib;
+            fileManager.upload(files, gid, location, single, imageOnly, done);
+        }
     }
 
     // 드래그 앤 드롭 파일 업로드 처리
