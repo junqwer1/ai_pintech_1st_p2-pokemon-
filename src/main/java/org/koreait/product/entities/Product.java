edb32475628cc728ae9194f3b22436263dde578f
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-06 18:02:20 +0900

    .

--- src/main/java/org/koreait/product/entities/Product.java
+++ src/main/java/org/koreait/product/entities/Product.java
@@ -2,8 +2,11 @@ package org.koreait.product.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.koreait.file.entities.FileInfo;
 import org.koreait.global.entities.BaseMemberEntity;
 import org.koreait.product.constants.DiscountType;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -11,6 +14,8 @@ public class Product extends BaseMemberEntity {
 
     @Id @GeneratedValue
     private Long seq;
+
+    private boolean open; // 상품 노출 여부
 
     @Column(length = 45, nullable = false)
     private String gid;
@@ -34,4 +39,13 @@ public class Product extends BaseMemberEntity {
     private double pointRate; // 적립률
 
     private int maxPoint; // 최대 적립금
+
+    @Transient
+    private List<FileInfo> mainImages; // 상품 상세 메인 이미지
+
+    @Transient
+    private List<FileInfo> listImages; // 목록 이미지
+
+    @Transient
+    private List<FileInfo> editorImages; // 상세설명 이미지
 }
