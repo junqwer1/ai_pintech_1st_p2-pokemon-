cc620ec4c0a405def4323819cc7d4ef505226a5e
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-07 16:50:10 +0900

    .

--- src/main/java/org/koreait/board/services/configs/BoardConfigInfoService.java
+++ src/main/java/org/koreait/board/services/configs/BoardConfigInfoService.java
@@ -1,13 +1,25 @@
 package org.koreait.board.services.configs;
 
+import com.querydsl.core.BooleanBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.koreait.admin.board.controllers.BoardConfigSearch;
 import org.koreait.board.entities.Board;
+import org.koreait.board.entities.QBoard;
 import org.koreait.board.exceptions.BoardNotFoundException;
 import org.koreait.board.repositories.BoardRepository;
 import org.koreait.global.paging.ListData;
+import org.koreait.global.paging.Pagination;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.desc;
 
 @Lazy
 @Service
@@ -15,6 +27,7 @@ import org.springframework.stereotype.Service;
 public class BoardConfigInfoService {
 
     private final BoardRepository boardRepository;
+    private final HttpServletRequest request;
 
     /**
      * 게시판 설정 하나 조회
@@ -41,7 +54,18 @@ public class BoardConfigInfoService {
         int limit = search.getLimit();
         limit = limit < 1 ? 20 : limit;
 
-        return null;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        QBoard board = QBoard.board;
+
+        Pageable pageable = PageRequest.of(page - 1, limit, Sort.by(desc("createdAt")));
+        Page<Board> data = boardRepository.findAll(andBuilder, pageable);
+
+        List<Board> items = data.getContent();
+        items.forEach(this::addInfo);
+
+        Pagination pagination = new Pagination(page, (int) data.getTotalElements(), 10, limit, request);
+
+        return new ListData<>(items, pagination);
     }
 
     private void addInfo(Board item) {
