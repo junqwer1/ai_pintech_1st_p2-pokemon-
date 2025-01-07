206c69db9ad9a43046df37eb40a94e2685e7096f
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2025-01-07 10:06:43 +0900

    .

--- src/main/resources/static/common/js/message.js
+++ src/main/resources/static/common/js/message.js
@@ -1,5 +1,12 @@
 const webSocket = new WebSocket(`ws://${location.host}/msg`);
 
 webSocket.addEventListener("message", function(data) {
-    console.log("message: ", data);
+    const el = commonLib.getMeta("user");
+
+    const { item, totalUnRead } = JSON.parse(data.data); //비구조 할당
+    let isShow = false;
+    if (item.notice || (email && email === item?.receiver?.email)) { // 공지사항
+        isShow = true;
+    }
+
 });
